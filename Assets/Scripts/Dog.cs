254ab80e5e6cca84d706ba7b46cb5161b3d908a0
ss-zun <zxq0158@gmail.com>
2024-04-03 15:36:38 +0900

    강아지 Food 반복 생성해주기

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public GameObject food;
+
+    void Start()
+    {
+        InvokeRepeating("MakeFood", 0f, 0.5f); // 강아지의 Food 반복 생성해주기
+    }
+
+    // 강아지의 Food 생성
+    void MakeFood()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        Instantiate(food, new Vector2(x, y), Quaternion.identity); // Quaternion.identity : 별도의 회전값을 주지 않겠다는 의미
+    }
+}
