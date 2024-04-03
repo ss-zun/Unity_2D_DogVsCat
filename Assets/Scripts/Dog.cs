a27795e1a248eed49f035fd1c787fe4569b5db62
ss-zun <zxq0158@gmail.com>
2024-04-03 15:46:15 +0900

    강아지가 화면 밖으로 나가지 않게 x값 제한

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -11,6 +11,24 @@ public class Dog : MonoBehaviour
         InvokeRepeating("MakeFood", 0f, 0.5f); // 강아지의 Food 반복 생성해주기
     }
 
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // 스크린상의 마우스 좌표를 월드 좌표로 변환
+        float x = mousePos.x;
+
+        // 강아지가 화면 밖으로 나가지 않게 x값 제한
+        if(x > 8.5f)
+        {
+            x = 8.5f;
+        }
+        if(x < -8.5f)
+        {
+            x = -8.5f;
+        }
+
+        transform.position = new Vector2(x, transform.position.y); // x값만 움직이고 y값은 현재 강아지의 y 위치
+    }
+
     // 강아지의 Food 생성
     void MakeFood()
     {
